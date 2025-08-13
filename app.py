921013c2d10198389071ef49aed9fe226d4d09e5
Ashik Shaji <ashikshaji4u@gmail.com>
2025-08-13 20:46:10 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LANGCHAIN_API = os.getenv("LANGCHAIN_API")
+LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT")
+GEMINI_API = os.getenv("GOOGLE_API_KEY")
+
+#prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        ("user", "Question:{question}")
+    ]
+)
+
+#streamlit framework
+st.title("Chatbot")
+st.write("Ask a question:")
+question = st.text_input("Question:")
+
+#gemini llm
+llm= ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    api_key=GEMINI_API
+)
+
+output_parser = StrOutputParser()
+chain=prompt | llm | output_parser
+
+if question:
+    with st.spinner("Generating response..."):
+        try:
+            response = chain.invoke({"question": question})
+            st.write("Response:", response)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
